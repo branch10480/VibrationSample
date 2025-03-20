e5c8898e4a9af55cd03c63a2dfd800cceeaf51a1
branch10480 <toshiharu.imaeda@gmail.com>
2025-03-20 17:10:20 +0900

    Initial Commit

---
+++ VibrationSample/VibrationSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibrationSampleApp.swift
+//  VibrationSample
+//
+//  Created by Toshiharu Imaeda on 2025/03/20.
+//
+
+import SwiftUI
+
+@main
+struct VibrationSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VibrationSampleTests/VibrationSampleTests.swift
@@ -0,0 +1,16 @@
+//
+//  VibrationSampleTests.swift
+//  VibrationSampleTests
+//
+//  Created by Toshiharu Imaeda on 2025/03/20.
+//
+
+import Testing
+
+struct VibrationSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
